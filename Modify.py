db364619b31697312b82cbccfc15eceee94e1020
kacper-marciniak <kacper.marciniak1998@gmail.com>
2023-11-10 14:32:49 +0100

    first commit

---
+++ Modify.py
@@ -0,0 +1,88 @@
+import numpy as np
+import os
+import cv2 as cv
+from data.utils import FDA_source_to_target_np, match_histogram
+from tqdm import tqdm
+
+def change_style(sPathImgSource: str, sPathImgTarget: str, sPathImgOutput: str|None = None, tTargetShape: tuple|None = None):
+    if sPathImgOutput is None: sPathImgOutput = sPathImgSource
+
+    aImgSource = cv.imread(sPathImgSource, cv.IMREAD_UNCHANGED)
+    aImgTarget = cv.imread(sPathImgTarget, cv.IMREAD_UNCHANGED)
+
+    if tTargetShape is None:
+        tTargetShape = aImgSource.shape
+    tTargetShape = (tTargetShape[1], tTargetShape[0])
+
+    aImgSource = cv.resize(aImgSource, tTargetShape, cv.INTER_CUBIC)
+    aImgTarget = cv.resize(aImgTarget, tTargetShape, cv.INTER_CUBIC)
+
+    aImgSource = np.array(aImgSource).astype(np.float32)
+    aImgTarget = np.array(aImgTarget).astype(np.float32)
+
+    aImgSource = aImgSource.transpose((2, 0, 1))
+    aImgTarget = aImgTarget.transpose((2, 0, 1))
+
+    aImgOutput = FDA_source_to_target_np(aImgSource, aImgTarget, L=BETA)
+
+    aImgOutput = aImgOutput.transpose((1,2,0))
+    aImgOutput = (aImgOutput - np.min(aImgOutput))/(np.max(aImgOutput)-np.min(aImgOutput))*255.0
+    aImgOutput = np.round(aImgOutput).astype(np.uint8)
+    cv.imwrite(sPathImgOutput, aImgOutput)
+
+def change_histogram(sPathImgSource: str, sPathImgTarget: str, sPathImgOutput: str|None = None, tTargetShape: tuple|None = None, bForceGrayScale: bool = False):
+    if sPathImgOutput is None: sPathImgOutput = sPathImgSource
+
+    aImgSource = cv.imread(sPathImgSource)
+    aImgTarget = cv.imread(sPathImgTarget)
+
+    if bForceGrayScale:
+        aImgSource = cv.cvtColor(aImgSource, cv.COLOR_BGR2GRAY)
+        aImgTarget = cv.cvtColor(aImgTarget, cv.COLOR_BGR2GRAY)
+
+    if tTargetShape is None:
+        tTargetShape = aImgSource.shape
+    tTargetShape = (tTargetShape[1], tTargetShape[0])
+
+    aImgSource = cv.resize(aImgSource, tTargetShape, cv.INTER_CUBIC)
+
+    aImgOutput = match_histogram(aImgSource, aImgTarget)
+
+    cv.imwrite(sPathImgOutput, aImgOutput)
+
+def generate_new_styles(sPathDataset: str, sPathToTargetSample: str, sPathToSave: str):
+    lSourceImages = [os.path.join(sPathDataset, sFile).lower() for sFile in os.listdir(sPathDataset) if sFile.lower().split('.')[-1] in ('jpg','png')]
+
+    print(f"Changing style of images in \'{sPathDataset}\' to the style of \'{sPathToTargetSample}\'")
+
+    for sImagePath in tqdm(lSourceImages):
+        change_style(
+            sImagePath,
+            sPathToTargetSample,
+            os.path.join(sPathToSave, os.path.basename(sImagePath).split('.')[0]+f"_hist_{os.path.basename(sPathToTargetSample.split('.')[0])}."+sImagePath.split('.')[-1]),
+        )
+
+def generate_new_histograms(sPathDataset: str, sPathToTargetSample: str, sPathToSave: str, bForceGrayScale: bool = False):
+    lSourceImages = [os.path.join(sPathDataset, sFile).lower() for sFile in os.listdir(sPathDataset) if sFile.lower().split('.')[-1] in ('jpg','png')]
+
+    print(f"Changing histogram of images in \'{sPathDataset}\' to the style of \'{sPathToTargetSample}\'")
+
+    for sImagePath in tqdm(lSourceImages):
+        change_histogram(
+            sImagePath,
+            sPathToTargetSample,
+            os.path.join(sPathToSave, os.path.basename(sImagePath).split('.')[0]+f"_hist_{os.path.basename(sPathToTargetSample.split('.')[0])}."+sImagePath.split('.')[-1]),
+            bForceGrayScale=bForceGrayScale
+        )
+
+BETA = 0.001
+
+PATH_IMAGES = 'images'
+PATH_SAMPLES = 'samples'
+PATH_OUT = 'out'
+
+GRAYSCALE = False
+
+for sSample in os.listdir(PATH_SAMPLES):
+    generate_new_styles(PATH_IMAGES, os.path.join(PATH_SAMPLES, sSample), PATH_OUT)
+    generate_new_histograms(PATH_IMAGES, os.path.join(PATH_SAMPLES, sSample), PATH_OUT, GRAYSCALE)
---
+++ README.md
@@ -0,0 +1,18 @@
+# Domain Adaptation for Machine Learning
+## FDA: Fourier Domain Adaptation
+## Histogram matching
+
+Modification of images for adaptation to new domains.
+
+*Modify.py* - Main script
+
+*images/* - input images to be transformed
+
+*samples/* - samples used in modification process
+
+*out/* - output images
+   
+
+**Acknowledgment**
+
+FDA adapted from [YanchaoYang's repository](https://github.com/YanchaoYang/FDA).
